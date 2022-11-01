9039b84a9bee0d9693de7bbcf1b018086f4d7370
Aleksey <a.e.sokolkov@gmail.com>
2022-11-02 01:52:59 +0500

    fix: deleted idea

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {TodoStore} from './stores/TodoStore';
 import TodoList from './components/TodoList';
 import {StoreContext} from './context/context';
+import './styles/style.scss';
 
 export const App = () => (
     <div>
